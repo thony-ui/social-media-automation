80bdf19fd6a607f5b1f464e602c3835e787530fb
thony-ui <anthonyhermanto1@gmail.com>
2025-08-24 15:41:17 +0800

    develop in docker production using npm run build

--- backend/src/lib/redis.ts
+++ backend/src/lib/redis.ts
@@ -1,14 +1,10 @@
 import Redis from "ioredis";
 require("dotenv").config();
 
-const isDev = process.env.NODE_ENV === "development";
-
 const redisClient = new Redis({
-  host: isDev ? "localhost" : process.env.REDIS_HOST,
-  port: isDev ? 6379 : Number(process.env.REDIS_PORT),
-  username: process.env.REDIS_USERNAME, // optional, only if using ACLs
-  password: process.env.REDIS_PASSWORD, // optional, only if set
-  // tls: !isDev ? {} : undefined, // uncomment if you need TLS in production
+  host: process.env.REDIS_HOST || "redis",
+  port: Number(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSWORD,
 });
 
 redisClient.on("error", (err) => {
